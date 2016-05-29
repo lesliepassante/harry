259f12112cb07d1b1f124301bf192fb181df9235
Leslie <leslie@fugue.co>
2016-05-29 14:39:59 -0400

    adding more logic

--- index.js
+++ index.js
@@ -22,12 +22,20 @@ Harry.prototype = Object.create(AlexaSkill.prototype);
 Harry.prototype.constructor = Harry;
 Harry.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
     console.log('Harry onLaunch requestId: ' + launchRequest.requestId + ', sessionId: ' + session.sessionId);
-    handleNewFactRequest(response);
+    handleNewSuggestion();
 };
 
 Harry.prototype.intentHandlers = {
     'GetSuggestionIntent': function (intent, session, response) {
-        handleNewSuggestionRequest(intent, response);
+        handleNewSuggestion(intent, response, session);
+    },
+
+    'TryAgainIntent': function (intent, session, response) {
+        handleTryAgain(intent, response, session);
+    },
+
+    'RecipeIntent': function (intent, session, response) {
+        handleRecipe(intent, response, session);
     },
 
     'AMAZON.HelpIntent': function (intent, session, response) {
@@ -55,21 +63,71 @@ Harry.prototype.intentHandlers = {
     }
 };
 
-function handleNewSuggestionRequest(intent, response) {
+function handleNewSuggestion(intent, response, session) {
+    var speechText = '';
     var ingredients = _.compact(_.map(intent.slots, function(n) {
         return n.value;
     }));
 
-    Craddock.ask({
-        ingredients: ingredients,
-        limit: '4',
-        season: 'summer'
-    }, function(err, res) {
-        console.log(err)
-        console.log(res)
-        var responseText = 'I suggest a gimlet.';
-        response.tellWithCard(responseText, 'Harry', responseText);
-    });
+    session.attributes.stage = 0;
+    session.attributes.suggestions = null;
+    session.attributes.currentCocktail = null
+
+    if(ingredients.length) {
+        Craddock.ask({ ingredients: ingredients }).then(function(res) {
+            session.attributes.stage = 1;
+            session.attributes.suggestions = res;
+            session.attributes.currentCocktail = 0;
+            speechText = formatSuggestion(res[0]);
+            response.tellWithCard(speechText, 'Harry', speechText, false);
+        }).catch(function(err) {
+            speechText = "I'm sorry, I couldn't find anything appropriate.";
+            response.tellWithCard(speechText, 'Harry', speechText, false);
+        });
+    } else {
+        speechText = 'What ingredients do you have available?';
+        response.tellWithCard(speechText, 'Harry', speechText, false);
+    }
+}
+
+function handleTryAgain(intent, response, session) {
+    var speechText = '';
+    
+    if(session.attributes.stage === 1) {
+        var current = session.attributes.currentCocktail;
+        var next = current + 1;
+
+        console.log("next: " + next)
+        if(session.attributes.suggestions && session.attributes.suggestions[next]) {
+            console.log(session.attributes.suggestions[next])
+            session.attributes.currentCocktail = next;
+            speechText = formatTryAgain(session.attributes.suggestions[next]);
+        } else {
+            speechText = "I'm sorry, those are all my suggestions. Please ask me again when you have different ingredients.";
+        }   
+    } else {
+        speechText = "What ingredients do you have?";
+    }
+
+    response.tellWithCard(speechText, 'Harry', speechText, false);
+}
+
+function handleRecipe(intent, response, session) {
+    var speechText = "I'm sorry, I'm not sure what is in that.";
+    response.tellWithCard(speechText, 'Harry', speechText, false);
+}
+
+function formatSuggestion(cocktail) {
+   return 'I suggest a ' + cocktail.name;
+}
+
+function formatTryAgain(cocktail) {
+   return 'Perhaps you would prefer a ' + cocktail.name;
+}
+
+
+function formatRecipe(cocktail) {
+
 }
 
 exports.handler = function (event, context) {
